b519da11767c69b2ad4b99e86a447e6733fd5db8
Zain khokhar <135629202+zainkhokhar831@users.noreply.github.com>
2025-12-03 22:32:30 +0500

    one file change

--- wp-content/themes/nestlaptops/templates/content-none.php
+++
@@ -1,4 +0,0 @@
-<h2 class="widgettitle">Nothing Found</h2>
-<div class="alert alert-warning">
-    <p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
-</div>
